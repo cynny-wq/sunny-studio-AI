97da8efe5c6d4173e7d6a7fb9de8b24e34b43c28
cynny-wq <cynthiawanjiku602@gmail.com>
2026-07-14 11:17:02 -0700

    Add files via upload

---
+++ storage.js
@@ -0,0 +1,7 @@
+function getProjects() {
+    return JSON.parse(localStorage.getItem("projects")) || [];
+}
+
+function saveProjects(projects) {
+    localStorage.setItem("projects", JSON.stringify(projects));
+}
